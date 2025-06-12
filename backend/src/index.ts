7567daafa07b0203a861be13ecf484abb5c99cc7
CecilioN7 <navarroscecilio@gmail.com>
2025-06-12 16:47:38 -0700

    middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,31 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import connectToDatabase from "./config/db";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import errorHandler from "./middleware/errorHandler";
+import { OK } from "./constants/http";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        status: "healthy"
+app.use(express.json()); //Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // From data allows nested objects and arrays
+app.use(
+    cors({
+        origin: APP_ORIGIN,
+        credentials: true, // required to send cookies or sessions
+    })
+);
+
+app.get("/", (req, res, next) => { 
+    res.status(OK).json({
+        status: "healthy",
     });
 });
 
-app.listen(4004, () => {
-    console.log(`Server is running on port 4004 in development environment`);
+app.use(errorHandler); // middleware will catch errors from the routes above
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT} in ${NODE_ENV} environment`);
+    await connectToDatabase();
 });
